eb6cc6f6639ca14fa05f083c3a520e8d69110f91
Ashu Goel <soxfan981@gmail.com>
2014-07-19 14:27:19 -0700

    added basic watchdog and daemonize functionality

--- desktop/app.py
+++ desktop/app.py
@@ -2,8 +2,45 @@ import logging
 import sys
 import time
 
+from daemonize import Daemonize
+import file_watcher
 from watchdog.events import LoggingEventHandler
 from watchdog.observers import Observer
 
+logger = logging.getLogger(__name__)
 
+def start_watchdog():
+    try:
+        file_watcher = file_watcher.FileWatcher(watch_path, logger)
+        file_watcher.start()
+    except Exception as e:
+        logger.exception("OOPS")
 
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: python app.py start|stop [args]'
+        sys.exit(1)
+    command = sys.argv[1]
+    pid = '/tmp/cumulonimbus.pid'
+    formatter = logging.Formatter(
+        "%(asctime)s %(threadName)-11s %(levelname)-10s %(message)s")
+    logger.setLevel(logging.DEBUG)
+    logger.propogate = False
+    fh = logging.FileHandler('log/cumulonimbus/cumulonimbus.log', 'w')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    keep_fds = [fh.stream.fileno()]
+    daemon = Daemonize(app="cumulonimbus",
+                       pid=pid,
+                       action=start_watchdog,
+                       keep_fds=keep_fds,
+                       logger=logger)
+    if command == 'start':
+        if len(sys.argv) < 3:
+            print 'Usage: python app.py start <path_to_watch>'
+            sys.exit(1)
+        watch_path = sys.argv[2]
+        daemon.start()
+    else:
+        daemon.exit()
