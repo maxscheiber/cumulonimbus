1d3d058b1d09d163051724bf66d529b9e5b66932
Lewis Ellis <ellisl@seas.upenn.edu>
2014-07-20 10:30:45 -0700

    More space button all done

--- chrome/ext/src/inject/inject.js
+++ chrome/ext/src/inject/inject.js
@@ -4,25 +4,42 @@ chrome.extension.sendMessage({accountId: ''}, function(response) {
 		clearInterval(readyStateCheckInterval);
 		console.log("Hello. This message was sent from scripts/inject.js");
 
-    var landingPath = '/m';
+    var helpPath = '/help';
+    var loginPath = '/login';
     var registerPath = '/m/register';
     var browsePath = '/m/browse';
     var accountId = '';
+    var proceed = false;
 
-    if (window.location.pathname === landingPath) {
+    if (window.location.pathname === helpPath) {
       accountId = window.location.search.slice(3);
+    }
+
+    if (window.location.pathname.slice(0, 9) === '/1/oauth2') {
+      accountId = 'waiting';
+      proceed = true;
     }
 
     chrome.runtime.sendMessage({accountId: accountId}, function(response) {
       accountId = response.accountId;
 
-      if (window.location.pathname === landingPath) {
-        document.querySelector('a[href="/m/register"] span').click();
+      if (!accountId || (accountId === 'waiting' && !proceed)) {
+        return;
+      } else if (window.location.pathname === helpPath) {
+        var link = document.querySelector('a#home-icon');
+        link.href = '/logout';
+        link.click();
+      } else if (window.location.pathname === loginPath) {
+        var link = document.querySelector('a#home-icon');
+        link.href = registerPath;
+        link.click();
       } else if (window.location.pathname === registerPath) {
+        var password = accountId.slice(0, 12);
+        var email = accountId.slice(12);
         document.getElementById('register_fname').value = 'Jimmy';
         document.getElementById('register_lname').value = 'Dean';
-        document.getElementById('register_email').value = 'jimmydeansammiches85@mailinator.com';
-        document.getElementById('register_password').value = 'jimmydean';
+        document.getElementById('register_email').value = email + '@mailinator.com';
+        document.getElementById('register_password').value = password;
         document.getElementById('tos_checkbox').checked = true;
         document.getElementById('register_submit').click();
       } else if (window.location.pathname === browsePath) {
