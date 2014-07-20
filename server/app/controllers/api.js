4d5bf6e38e5659a512110b129e77e34d20c799f2
Lewis Ellis <ellisl@seas.upenn.edu>
2014-07-19 17:48:24 -0700

    File adding endpoint, all API routes in place

--- server/app/controllers/api.js
+++ server/app/controllers/api.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
-    passport = require('passport');
+    passport = require('passport'),
+    _ = require('underscore');
 
 var User = mongoose.model('User');
 var Account = mongoose.model('Account');
@@ -19,6 +20,7 @@ exports.login = function(req, res, next) {
       }
 
       return res.json({
+        status: 'success',
         message: 'Logged in successfully',
         username: user.username
       });
@@ -26,24 +28,114 @@ exports.login = function(req, res, next) {
   })(req, res, next);
 };
 
-exports.instructions = function(req, res) {
-  var body = req.body;
-  var size = body.size;
+exports.pathListing = function(req, res) {
+  return res.json({message: 'not completed yet'})
+}
 
-  Account.getMostFree(req.user._id, function(err, account) {
-    if (account.free < body.size) {
-      res.json(403, {error: 'Not enough room'});
+function reduceAccount(account) {
+  return {
+    id: account._id,
+    name: account.name,
+    provider: account.provider,
+    token: account.oauthToken
+  };
+}
+
+exports.accounts = function(req, res) {
+  User.load(req.user.username, function(err, user) {
+    if (err) {
+      return res.json(404, {message: 'User not found'});
     }
 
-    // return oauth token, provider, account
+    accounts = _.map(user.accounts, reduceAccount);
+
     return res.json({
-      token: account.oauthToken,
-      provider: account.provider,
-      account: account.name
+      status: 'success',
+      accounts: accounts
     });
   });
 }
 
-exports.tokens = function(req, res) {
+exports.instructionsNew = function(req, res) {
+  var size = req.body.size;
+
+  Account.getMostFree(req.user._id, function(err, account) {
+    if (account.free < body.size) {
+      return res.json(403, {error: 'Not enough room'});
+    }
+
+    return res.json({
+      status: 'success',
+      account: reduceAccount(account)
+    });
+  });
+}
+
+exports.instructionsMove = function(req, res) {
+  return res.json({message: 'not done yet'});
+}
+
+exports.instructionsModify = function(req, res) {
+  return res.json({message: 'not done yet'});
+}
+
+exports.instructionsDelete = function(req, res) {
+  return res.json({message: 'not done yet'});
+}
+
+exports.updateNew = function(req, res) {
+  var post = req.body;
+
+  var filename = post.filename;
+  var path = post.path;
+  var provider = post.provider;
+  var cloudId = post.cloudId;
+  var size = post.size;
+  var accountId = post.accountId;
+  var now = Date.now();
+
+  if (!(filename && path && provider && cloudId && size && accountId)) {
+    return res.json('403', {
+      status: 'error',
+      message: 'Include all fields: filename path provider cloudId size accountId'
+    })
+  }
+
+  var file = new File({
+    name: filename,
+    path: path,
+    provider: provider,
+    cloudId: cloudId,
+    size: size,
+    user: req.user._id,
+    account: accountId,
+    createDate: now,
+    changeDate: now
+  });
+
+  file.save(function(err) {
+    if (err) {
+      return res.json(500, {
+        status: 'error',
+        message: 'Failed to add file'
+      });
+    }
+
+    return res.json({
+      status: 'success',
+      message: 'File added successfully'
+    });
+  })
+}
+
+exports.updateMove = function(req, res) {
+  return res.json({message: 'not done yet'});
+}
+
+exports.updateModify = function(req, res) {
+  return res.json({message: 'not done yet'});
+}
+
+exports.updateDelete = function(req, res) {
   return res.json({message: 'not done yet'});
 }
--- server/app/routes.js
+++ server/app/routes.js
@@ -9,19 +9,26 @@ module.exports = function(app) {
   app.get('/about', page.about);
   app.get('/login', page.login);
   app.get('/logout', page.logout);
+  app.post('/login', user.processLogin);
+  app.post('/register', user.register);
 
-  app.post('/api/login', api.login)
-
-  //app.get('/api/folder/:path', auth.apiLogin, api.pathListing);
-  app.get('/api/tokens', auth.apiLogin, api.tokens);
-  app.post('/api/instructions', auth.apiLogin, api.instructions);
-  //app.post('/api/mkdir', auth.apiLogin, api.mkdir);
+  app.get('/user/:username/accounts', user.accounts);
+  app.get('/user/:username', user.show);
 
   app.get('/account/new', auth.requireLogin, account.create);
   app.post('/account/new', account.create);
   app.get('/account/:account', account.show);
-  app.get('/user/:username/accounts', user.accounts);
-  app.get('/user/:username', user.show);
-  app.post('/login', user.processLogin);
-  app.post('/register', user.register);
+
+
+  app.post('/api/login', api.login)
+  app.get('/api/folder/:path', auth.apiLogin, api.pathListing);
+  app.get('/api/accounts', auth.apiLogin, api.accounts);
+  app.post('/api/instructions/new', auth.apiLogin, api.instructionsNew);
+  app.post('/api/instructions/move', auth.apiLogin, api.instructionsMove);
+  app.post('/api/instructions/modify', auth.apiLogin, api.instructionsModify);
+  app.post('/api/instructions/delete', auth.apiLogin, api.instructionsDelete);
+  app.post('/api/update/new', auth.apiLogin, api.updateNew);
+  app.post('/api/update/move', auth.apiLogin, api.updateMove);
+  app.post('/api/update/modify', auth.apiLogin, api.updateModify);
+  app.post('/api/update/delete', auth.apiLogin, api.updateDelete);
 };
